2660acf4e182599482a38ab5a8b88aac6228aa6f
teteuhere <matheuss.cirq@outlook.com>
2023-12-26 20:21:48 -0300

    Third challenge

---
+++ castingNumbers/README.md
@@ -0,0 +1,4 @@
+## Average of numbers
+This code is a casting between int and double.
+
+## bye
---
+++ castingNumbers/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        double go = 25.8;
+        int gojo = (int) go;
+        System.out.println("Casting for a integre number: " + gojo);
+    }
+}
